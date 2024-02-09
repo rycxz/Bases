ca7687e2b98f6220fa2c9566c2250fd0613d7b82
Ricardo <recuricardo@gmail.com>
2024-02-09 16:47:48 +0100

    cambios base de preatica

--- paragit/Practica/consultas.sql
+++ paragit/Practica/consultas.sql
@@ -1,2 +1,9 @@
 use crunchy;
-
+ 
+ /*los nombres de  listas creadas los usuarios con su nombre y su fecha de creaciion y los animes que conienen */
+ 
+select lista.nombre as 'nombre de la lista' ,usuario.nickname as 'nickname del usuario',lista.fecha_de_creacion as 'fecha de creacion de la lista',anime.nombre as 'nombre del anime' 
+from lista
+join usuario on usuario.id=lista.id_usuario
+join lista_anime on lista_anime.id_lista = lista.id
+join anime on anime.id = lista_anime.id_anime;
